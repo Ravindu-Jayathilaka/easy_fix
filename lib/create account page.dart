d4d5a70efbe42c83682d0b7bd335ac15e4513e58
Ravindu Jayathilaka <57854539+Ravindu-Jayathilaka@users.noreply.github.com>
2022-03-09 00:30:47 +0530

    login page

---
+++ lib/create account page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CreateAccount extends StatefulWidget {
+  const CreateAccount({Key? key}) : super(key: key);
+
+  @override
+  State<CreateAccount> createState() => _CreateAccountState();
+}
+
+class _CreateAccountState extends State<CreateAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(
+            children: const [
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Name',
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Email',
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Password',
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Mobile Number',
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
